2ee981f9f4d850f59c3c743ccd0eeb397e77b404
Russell <russell.tarpley@gmail.com>
2017-01-05 13:17:02 -0600

    chore(initial project state)
    setting project up. dev starting point

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # mdl-currency-usd
-Material Design Lite currency-usd component (https://github.com/google/material-design-lite)
+An Material Design Lite phone textfield implementation for capturing user entered US currency values (https://github.com/google/material-design-lite)
 
 [![Bower Version](https://img.shields.io/bower/v/mdl-currency-usd.svg)](https://github.com/rathxxx/mdl-currency-usd)
 [![NPM Version](https://img.shields.io/npm/v/mdl-currency-usd.svg)](https://www.npmjs.com/package/mdl-currency-usd)
@@ -10,6 +10,80 @@ Material Design Lite currency-usd component (https://github.com/google/material-
 [![bitHound Dev Dependencies](https://www.bithound.io/github/rathxxx/mdl-currency-usd/badges/devDependencies.svg)](https://www.bithound.io/github/rathxxx/mdl-currency-usd/master/dependencies/npm)
 [![bitHound Code](https://www.bithound.io/github/rathxxx/mdl-currency-usd/badges/code.svg)](https://www.bithound.io/github/rathxxx/mdl-currency-usd)
 
-> A custom implementation of a currency-usd component for [Material Design Lite](https://github.com/google/material-design-lite)
+> A custom textfield implementation of a US currency component for [Material Design Lite](https://github.com/google/material-design-lite)
 
+## Install
 
+Via npm:
+
+````
+npm install mdl-currency-usd
+````
+
+Then include in your html:
+
+````
+<link rel="stylesheet" href="./bower_components/mdl-currency-usd/dist/mdl-currency-usd.min.css">
+...
+<script src="./bower_components/mdl-currency-usd/dist/mdl-currency-usd.min.js"></script>
+````
+
+## Basic use
+To use any MDL component, you must include the minified CSS and JavaScript files using standard relative-path references in the `<head>` section of the page, as described in the MDL Introduction.
+
+### To include a MDL **currency-usd textfield** component:
+
+&nbsp;1. Code a `<div>` element to hold the phone text field.
+```html
+<div>
+...
+</div>
+```
+&nbsp;2. Inside the div, code an `<input>` element add an `id` attribute of your choice.
+```html
+<div>
+  <input type="text" id="sample3">
+</div>
+```
+&nbsp;3. Also inside the div, after the `<input>` field, code a `<label>` element with a `for` attribute whose value matches the `input` element's `id` value, and a short string to be used as the field's placeholder text.
+```html
+<div>
+		<input type="text" id="sample3">
+        <label for="sample3">currency Example...</label>
+</div>
+```
+&nbsp;4. Add one or more MDL classes, separated by spaces, to the div container, input field, input label, and error message using the `class` attribute.
+```html
+<div class="mdl-currency-usd mdl-js-currency-usd mdl-currency-usd--floating-label">
+        <input class="mdl-currency-usd__input" type="text" id="sample3">
+        <label class="mdl-currency-usd__label" for="sample3">currency Example...</label>
+</div>
+```
+The phone textfield component is ready for use.
+
+#### Examples
+
+phone field with a standard label.
+```html
+<div class="mdl-currency-usd mdl-js-currency-usd">
+        <input class="mdl-currency-usd__input" type="text" id="sample1">
+        <label class="mdl-currency-usd__label" for="sample1">currency Example...</label>
+</div>
+```
+
+phone field with a floating label.
+```html
+<div class="mdl-currency-usd mdl-js-currency-usd mdl-currency-usd--floating-label">
+        <input class="mdl-currency-usd__input" type="text" id="sample4">
+        <label class="mdl-currency-usd__label" for="sample4">currency Example...</label>
+</div>
+```
+
+phone field with a standard label, and error message.
+```html
+<div class="mdl-currency-usd mdl-js-currency-usd">
+        <input class="mdl-currency-usd__input" type="text" id="sample4">
+        <label class="mdl-currency-usd__label" for="sample4">currency Example...</label>
+		<span class="mdl-currency-usd__error">$9.99</span>
+</div>
+```
---
+++ dist/mdl-currency-usd.js
@@ -0,0 +1,256 @@
+(function() {
+    'use strict';
+
+    /**
+     * Class constructor for SSN Textfield MDL component.
+     * Implements MDL component design pattern defined at:
+     * https://github.com/jasonmayes/mdl-component-design-pattern
+     *
+     * @constructor
+     * @param {HTMLElement} element The element that will be upgraded.
+     */
+    var MaterialCurrencyTextfield = function MaterialCurrencyTextfield(element) {
+        this.element_ = element;
+        this.rawValue = "";
+        // Initialize instance.
+        this.init();
+    };
+    window['MaterialCurrencyTextfield'] = MaterialCurrencyTextfield;
+
+    /**
+     * Store constants in one place so they can be updated easily.
+     *
+     * @enum {string | number}
+     * @private
+     */
+    MaterialCurrencyTextfield.prototype.Constant_ = {};
+
+    /**
+     * Store strings for class names defined by this component that are used in
+     * JavaScript. This allows us to simply change it in one place should we
+     * decide to modify at a later date.
+     *
+     * @enum {string}
+     * @private
+     */
+    MaterialCurrencyTextfield.prototype.CssClasses_ = {
+        LABEL: 'mdl-currency-usd__label',
+        INPUT: 'mdl-currency-usd__input',
+        IS_DIRTY: 'is-dirty',
+        IS_FOCUSED: 'is-focused',
+        IS_DISABLED: 'is-disabled',
+        IS_INVALID: 'is-invalid',
+        IS_UPGRADED: 'is-upgraded'
+    };
+
+    /**
+     * Handle focus.
+     *
+     * @param {Event} event The event that fired.
+     * @private
+     */
+    MaterialCurrencyTextfield.prototype.onFocus_ = function (event) {
+        this.element_.classList.add(this.CssClasses_.IS_FOCUSED);
+    };
+
+    /**
+     * Handle lost focus.
+     *
+     * @param {Event} event The event that fired.
+     * @private
+     */
+    MaterialCurrencyTextfield.prototype.onBlur_ = function (event) {
+        this.element_.classList.remove(this.CssClasses_.IS_FOCUSED);
+    };
+
+    MaterialCurrencyTextfield.prototype.onChange_ = function (event) {
+        var pattern = /^\(?(\d{3})\)?\u0020?\.?-?(\d{3})\u0020?\.?-?\u0020?(\d{4})$/;
+        //Test the input string for basic format (optional '-')
+        if (!pattern.test(this.input_.value)) {
+            this.element_.classList.add(this.CssClasses_.IS_INVALID);
+            return false;
+        } else {
+            var matches = pattern.exec(this.input_.value);
+            this.rawValue = matches[1] + matches[2] + matches[3];
+            this.input_.value = "(" + matches[1] + ") " + matches[2] + " - " + matches[3];
+        }
+    };
+
+    /**
+     * Handle reset event from out side.
+     *
+     * @param {Event} event The event that fired.
+     * @private
+     */
+    MaterialCurrencyTextfield.prototype.onReset_ = function (event) {
+        this.updateClasses_();
+    };
+
+    /**
+     * Handle class updates.
+     *
+     * @private
+     */
+    MaterialCurrencyTextfield.prototype.updateClasses_ = function () {
+        this.checkDisabled();
+        this.checkValidity();
+        this.checkDirty();
+        this.checkFocus();
+    };
+
+    // Public methods.
+
+    /**
+     * Check the disabled state and update field accordingly.
+     *
+     * @public
+     */
+    MaterialCurrencyTextfield.prototype.checkDisabled = function () {
+        if (this.input_.disabled) {
+            this.element_.classList.add(this.CssClasses_.IS_DISABLED);
+        } else {
+            this.element_.classList.remove(this.CssClasses_.IS_DISABLED);
+        }
+    };
+    MaterialCurrencyTextfield.prototype['checkDisabled'] =
+        MaterialCurrencyTextfield.prototype.checkDisabled;
+
+    /**
+     * Retrieve the raw value of the field
+     * @returns {string} unmasked value
+     */
+    MaterialCurrencyTextfield.prototype.getValue = function () {
+        return this.rawValue;
+    };
+    MaterialCurrencyTextfield.prototype['getValue'] =
+        MaterialCurrencyTextfield.prototype.getValue;
+
+    /**
+    * Check the focus state and update field accordingly.
+    *
+    * @public
+    */
+    MaterialCurrencyTextfield.prototype.checkFocus = function () {
+        if (Boolean(this.element_.querySelector(':focus'))) {
+            this.element_.classList.add(this.CssClasses_.IS_FOCUSED);
+        } else {
+            this.element_.classList.remove(this.CssClasses_.IS_FOCUSED);
+        }
+    };
+    MaterialCurrencyTextfield.prototype['checkFocus'] =
+      MaterialCurrencyTextfield.prototype.checkFocus;
+
+    /**
+     * Check the validity state and update field accordingly.
+     *
+     * @public
+     */
+    MaterialCurrencyTextfield.prototype.checkValidity = function () {
+        if (this.input_.validity) {
+            if (this.input_.validity.valid) {
+                this.element_.classList.remove(this.CssClasses_.IS_INVALID);
+            } else {
+                this.element_.classList.add(this.CssClasses_.IS_INVALID);
+            }
+        }
+    };
+    MaterialCurrencyTextfield.prototype['checkValidity'] =
+        MaterialCurrencyTextfield.prototype.checkValidity;
+
+    /**
+     * Check the dirty state and update field accordingly.
+     *
+     * @public
+     */
+    MaterialCurrencyTextfield.prototype.checkDirty = function () {
+        if (this.input_.value && this.input_.value.length > 0) {
+            this.element_.classList.add(this.CssClasses_.IS_DIRTY);
+        } else {
+            this.element_.classList.remove(this.CssClasses_.IS_DIRTY);
+        }
+    };
+    MaterialCurrencyTextfield.prototype['checkDirty'] =
+        MaterialCurrencyTextfield.prototype.checkDirty;
+
+    /**
+     * Disable text field.
+     *
+     * @public
+     */
+    MaterialCurrencyTextfield.prototype.disable = function () {
+        this.input_.disabled = true;
+        this.updateClasses_();
+    };
+    MaterialCurrencyTextfield.prototype['disable'] = MaterialCurrencyTextfield.prototype.disable;
+
+    /**
+     * Enable text field.
+     *
+     * @public
+     */
+    MaterialCurrencyTextfield.prototype.enable = function () {
+        this.input_.disabled = false;
+        this.updateClasses_();
+    };
+    MaterialCurrencyTextfield.prototype['enable'] = MaterialCurrencyTextfield.prototype.enable;
+
+    /**
+     * Update text field value.
+     *
+     * @param {string} value The value to which to set the control (optional).
+     * @public
+     */
+    MaterialCurrencyTextfield.prototype.change = function (value) {
+
+        this.input_.value = value || '';
+        this.updateClasses_();
+    };
+    MaterialCurrencyTextfield.prototype['change'] = MaterialCurrencyTextfield.prototype.change;
+
+    /**
+   * Initialize element.
+   */
+    MaterialCurrencyTextfield.prototype.init = function () {
+
+        if (this.element_) {
+            this.label_ = this.element_.querySelector('.' + this.CssClasses_.LABEL);
+            this.input_ = this.element_.querySelector('.' + this.CssClasses_.INPUT);
+
+            if (this.input_) {
+                this.boundUpdateClassesHandler = this.updateClasses_.bind(this);
+                this.boundFocusHandler = this.onFocus_.bind(this);
+                this.boundBlurHandler = this.onBlur_.bind(this);
+                this.boundResetHandler = this.onReset_.bind(this);
+                this.boundChangeHandler = this.onChange_.bind(this);
+                this.input_.addEventListener('input', this.boundUpdateClassesHandler);
+                this.input_.addEventListener('focus', this.boundFocusHandler);
+                this.input_.addEventListener('blur', this.boundBlurHandler);
+                this.input_.addEventListener('reset', this.boundResetHandler);
+                this.input_.addEventListener('change', this.boundChangeHandler);
+
+                var invalid = this.element_.classList.contains(this.CssClasses_.IS_INVALID);
+                this.updateClasses_();
+                this.element_.classList.add(this.CssClasses_.IS_UPGRADED);
+                if (invalid) {
+                    this.element_.classList.add(this.CssClasses_.IS_INVALID);
+                }
+                if (this.input_.hasAttribute('autofocus')) {
+                    this.element_.focus();
+                    this.checkFocus();
+                }
+                if (this.input_.value.length > 0) {
+                    this.onChange_();
+                }
+            }
+        }
+    };
+
+    // The component registers itself. It can assume componentHandler is available
+    // in the global scope.
+    componentHandler.register({
+        constructor: MaterialCurrencyTextfield,
+        classAsString: 'MaterialCurrencyTextfield',
+        cssClass: 'mdl-js-currency-usd',
+        widget: true
+    });
+})();
